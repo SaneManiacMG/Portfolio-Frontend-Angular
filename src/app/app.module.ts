a716e2a6a6bb79228e07898c65fe946cf8e36892
SaneManiacMG <mmoroane@hotmail.com>
2023-06-14 09:35:37 +0200

    Removed contents of the app.html Created Navbar, Sidebar, Footer, MainContainer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule,
