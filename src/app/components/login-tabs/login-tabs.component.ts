532322349ddc7ec12aaed666b698e560ab0a44c0
SaneManiacMG <mmoroane@hotmail.com>
2023-06-28 14:29:54 +0200

    Created logic for login-tabs

--- src/app/components/login-tabs/login-tabs.component.ts
+++ src/app/components/login-tabs/login-tabs.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-tabs',
   templateUrl: './login-tabs.component.html',
-  styleUrls: ['./login-tabs.component.css']
+  styleUrls: ['../../app.component.css'],
 })
 export class LoginTabsComponent implements OnInit {
+  loginTab: boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  switchToLogin() {
+    this.loginTab = true;
   }
 
+  switchToRegister() {
+    this.loginTab = false;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
